fe2c494f97e284e8e45479162eb1cb572e45afec
Seda Ertuğrul <ertugrulseda@gmail.com>
2022-11-20 20:45:32 +0300

    changes

--- src/stories/myButton.stories.jsx
+++ src/stories/myButton.stories.jsx
@@ -19,7 +19,7 @@ export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   primary: true,
-  label: 'Çok güzelsin Seda',
+  label: 'Denemeeee',
 };
 
 export const Secondary = Template.bind({});
